d54d77ed0dad275a8d2f4a059241a74ca9180aec
Riri <jamesriri03@gmail.com>
2024-02-23 23:59:06 +0300

    Learn about macros and more advanced concept on preprocessors

--- source/chapters/functions/multiple/main.cpp
+++ source/chapters/functions/multiple/main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
-#include "add.cpp"
+#include "add.h"
 
-int add(int x, int y); // Forward function declaration of add().
+//int add(int x, int y); // Forward function declaration of add().
 int main()
 {
+    std::cout << "Enter first integer number: ";
     int x{6};
+    std::cin >> x;
+
+    std::cout << "Enter your second number: ";
     int y{4};
+    std::cin >> y;
 
     // add() works pretty well due to the fact that declaration of it has been made, otherwise it would raise a compile error.
     std::cout << "The sum of " << x << " and " << y << " = " << add(x, y) << '\n'; 
---
+++ source/exercises/quickie.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+#define MY_ROOT "Bob"
+#define NEW_MACRO 
+
+int main()
+{
+#ifdef MY_ROOT
+    std::cout << "Am I remembering shit?! " << MY_ROOT << '\n';
+#endif
+
+#if defined(NEW_MACRO)
+    std::cout << "This does the same as #ifdef but it's more C++-style syntax oriented.\n";
+#endif 
+
+#ifndef PRINT_NAME
+    std::cout << "This should be printed out.\n";
+#endif
+
+#if !defined(MY_ROOT)
+    std::cout <<  "This does the same as #ifndef but uses a more C++-style syntax. \n";
+#endif
+
+#if 1
+    int x{};
+    std::cout << "Enter any number: ";
+    std::cin >> x;
+    std::cout << "You entered " << x << " and " << x << "^2 is: " << x * x << '\n';
+#endif
+
+}
